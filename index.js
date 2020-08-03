5abbc72cd28e208305772575f999b3fe0acca88f
Christophe Coevoet <stof@notk.org>
2020-08-04 01:14:27 +0200

    Add more precise JSDoc for objects used as maps
    This documents that they are used in such a way, with the type of the
    values.

--- index.js
+++ index.js
@@ -350,7 +350,7 @@ class Encore {
      * })
      * ```
      *
-     * @param {object} aliases
+     * @param {Object<string, string>} aliases
      *
      * @returns {Encore}
      */
@@ -742,7 +742,7 @@ class Encore {
      *  This is useful for older packages, that might
      *  expect jQuery (or something else) to be a global variable.
      *
-     * @param {object} variables
+     * @param {Object<string, string|string[]>} variables
      * @returns {Encore}
      */
     autoProvideVariables(variables) {
